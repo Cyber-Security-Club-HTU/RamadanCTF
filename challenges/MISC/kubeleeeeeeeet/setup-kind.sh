16ced9fe5471149dff0b1386bb90a49e5ec04517
smadi0x86 <smadixd@gmail.com>
2024-03-19 01:36:15 +0300

    Enhance misconfigs for better understanding around kubelet

--- challenges/MISC/kubeleeeeeeeet/setup-kind.sh
+++ challenges/MISC/kubeleeeeeeeet/setup-kind.sh
@@ -9,18 +9,13 @@ sleep 10
 # Create the KinD cluster
 kind create cluster --wait 5m
 
-# Misconfigure the kubelet by setting an incorrect clientCAFile path
-docker exec kind-control-plane sh -c 'sed -i "s|/etc/kubernetes/pki/ca.crt|/wrong/path/to/ca.crt|g" /var/lib/kubelet/config.yaml && systemctl restart kubelet'
-
-# Install micro editor in the KinD control-plane node
+docker exec kind-control-plane sh -c 'sed -i "s|/etc/kubernetes/manifests|/etc/kubernetes/invalid-manifests|g" /var/lib/kubelet/config.yaml && systemctl restart kubelet'
+docker exec kind-control-plane sh -c 'sed -i "s|https://kind-control-plane:6443|https://kind-control-plane:7443|g" /etc/kubernetes/kubelet.conf'
 MICRO_VERSION="2.0.10"
 docker exec kind-control-plane sh -c "curl -L https://github.com/zyedidia/micro/releases/download/v${MICRO_VERSION}/micro-${MICRO_VERSION}-linux64.tar.gz | tar -xz -C /usr/local/bin --strip-components=1 micro-${MICRO_VERSION}/micro"
 
-# Copy the flag-pod.yaml to the control-plane node
-docker cp flag-pod.yaml kind-control-plane:/etc/kubernetes/manifests/flag-pod.yaml
+docker cp flag-pod.yaml kind-control-plane:/etc/flag-pod.yaml
 
-# Set permissions for the flag-pod.yaml to be readable only by root
-docker exec kind-control-plane sh -c 'chmod 600 /etc/kubernetes/manifests/flag-pod.yaml'
+docker exec kind-control-plane sh -c 'chmod 600 /etc/flag-pod.yaml'
 
-# Provide an interactive shell to the user
 exec docker exec -it $(docker ps -qf "name=kind-control-plane") bash
